fd73f6291d0f1b81e0297b76ff07633c02c1afae
Skuld Norniern <43695854+Archerko01@users.noreply.github.com>
2020-11-01 20:30:15 +0900

    tweaking

--- cordium.go
+++ cordium.go
@@ -1,23 +1,17 @@
 package main
 
 import (
-	"fmt"
-
-	"fyne.io/fyne"
-	"fyne.io/fyne/app"
-	"github.com/SkuldNorniern/Cordium/frame"
 	"github.com/SkuldNorniern/Cordium/network"
-	"rsc.io/quote"
 )
 
 func main() {
-	a := app.NewWithID("Wormhole")
+	/*a := app.NewWithID("Wormhole")
 	w := a.NewWindow("Wormhole")
 	w.SetMaster()
 	fmt.Println(quote.Hello())
 	frame.DialogScreen(w)
-	network.Hostsmb()
 	w.Resize(fyne.NewSize(600, 400))
 	w.CenterOnScreen()
-	w.ShowAndRun()
+	w.ShowAndRun()*/
+	network.Hostsmb()
 }
